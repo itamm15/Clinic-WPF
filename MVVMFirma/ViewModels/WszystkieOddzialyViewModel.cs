fed458696892759a5284b4a3b1e90134e846e52d
itamm15 <osinski.mateusz15@gmail.com>
2024-11-29 12:58:54 +0100

    list oddzialy

--- MVVMFirma/ViewModels/WszystkieOddzialyViewModel.cs
+++ MVVMFirma/ViewModels/WszystkieOddzialyViewModel.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVMFirma.Models.Entities;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkieOddzialyViewModel : WorkspaceViewModel
+    public class WszystkieOddzialyViewModel : WszystkieViewModel<Oddzialy>
     {
-        public WszystkieOddzialyViewModel()
+        public WszystkieOddzialyViewModel() : base()
         {
             base.DisplayName = "Oddzialy";
+        }
+
+        public override void Load()
+        {
+            List = new ObservableCollection<Oddzialy>(
+                przychodniaEntities.Oddzialy.ToList() 
+               );
         }
     }
 }
