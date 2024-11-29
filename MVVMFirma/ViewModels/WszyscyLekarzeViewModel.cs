70b17a575e8c637c41426791a6a90447501b3e2a
itamm15 <osinski.mateusz15@gmail.com>
2024-11-29 13:26:23 +0100

    list lekarze

--- MVVMFirma/ViewModels/WszyscyLekarzeViewModel.cs
+++ MVVMFirma/ViewModels/WszyscyLekarzeViewModel.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVMFirma.Models.Entities;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszyscyLekarzeViewModel : WorkspaceViewModel
+    public class WszyscyLekarzeViewModel : WszystkieViewModel<Lekarze>
     {
-        public WszyscyLekarzeViewModel()
+        public WszyscyLekarzeViewModel() : base()
         {
             base.DisplayName = "Wszyscy lekarze";
+        }
+
+        public override void Load()
+        {
+            List = new System.Collections.ObjectModel.ObservableCollection<Lekarze>(
+                przychodniaEntities.Lekarze.ToList()
+                );
         }
     }
 }
