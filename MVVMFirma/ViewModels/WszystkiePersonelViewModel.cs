5680b35875fdd7ec5fa9ab6506933f19260caffc
itamm15 <osinski.mateusz15@gmail.com>
2024-11-29 13:19:58 +0100

    list personel

--- MVVMFirma/ViewModels/WszystkiePersonelViewModel.cs
+++ MVVMFirma/ViewModels/WszystkiePersonelViewModel.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVMFirma.Models.Entities;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkiePersonelViewModel : WorkspaceViewModel
+    public class WszystkiePersonelViewModel : WszystkieViewModel<Personel>
     {
-        public WszystkiePersonelViewModel()
+        public WszystkiePersonelViewModel() : base()
         {
             base.DisplayName = "Personel";
+        }
+
+        public override void Load()
+        {
+            List = new ObservableCollection<Personel>(
+                przychodniaEntities.Personel.ToList()
+                );
         }
     }
 }
