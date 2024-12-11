e5c49c1e028c25c77732d97654a847704048968d
itamm15 <osinski.mateusz15@gmail.com>
2024-12-11 22:44:47 +0100

    list all ReceptyLeki with assocs

---
+++ MVVMFirma/Models/EntitiesForView/ReceptaLekiForAllView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.EntitiesForView
+{
+    public class ReceptaLekiForAllView
+    {
+        public int ReceptaLekiId { get; set; }
+        public String Dawkowanie {  get; set; }
+
+        // Lek
+
+        public String LekNazwa { get; set; }
+        public String LekTyp { get; set; }  
+
+        // Recepta
+        public int? ReceptaId { get; set; }
+
+        // Pacjent
+        public String PacjentImieNazwisko { get; set; }
+    }
+}
--- MVVMFirma/ViewModels/WszystkieReceptyLekiViewModel.cs
+++ MVVMFirma/ViewModels/WszystkieReceptyLekiViewModel.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVMFirma.Models.EntitiesForView;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkieReceptyLekiViewModel : WorkspaceViewModel
+    public class WszystkieReceptyLekiViewModel : WszystkieViewModel<ReceptaLekiForAllView>
     {
-        public WszystkieReceptyLekiViewModel()
+        public WszystkieReceptyLekiViewModel() : base()
         {
             base.DisplayName = "Wszystkie recepty-leki";
+        }
+
+        public override void Load()
+        {
+            List = new ObservableCollection<ReceptaLekiForAllView>(
+                from receptaLek in przychodniaEntities.ReceptaLeki
+                select new ReceptaLekiForAllView
+                {
+                    ReceptaLekiId = receptaLek.ReceptaLekiId,
+                    Dawkowanie = receptaLek.Dawkowanie,
+                    LekNazwa = receptaLek.Leki.NazwaLeku,
+                    LekTyp = receptaLek.Leki.TypLeku,
+                    ReceptaId = receptaLek.ReceptaId,
+                    PacjentImieNazwisko = receptaLek.Recepty.Pacjenci.ImieNazwisko
+                }
+                );
         }
     }
 }
