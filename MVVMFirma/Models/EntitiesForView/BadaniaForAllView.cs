094519e4d5acf9760094da65dcd2af0d98fbed4d
itamm15 <osinski.mateusz15@gmail.com>
2024-12-11 21:04:14 +0100

    list all Badania with assocs

---
+++ MVVMFirma/Models/EntitiesForView/BadaniaForAllView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.EntitiesForView
+{
+    public class BadaniaForAllView
+    {
+        public int BadanieId {  get; set; } 
+        public String TypBadania { get; set; }
+        public DateTime? DataBadania { get; set; }
+
+        // Pacjent 
+        public String PacjentImieNazwisko { get; set; }
+        public DateTime? PacjentDataUrodzenia { get; set; }
+        public String PacjentMiasto { get; set; }
+
+        // Lekarz
+        public String LekarzImieNazwisko { get; set; }
+        public String LekarzSpecjalizacja { get; set; }
+        public String LekarzNumerTelefonu { get; set; }
+    }
+}
--- MVVMFirma/ViewModels/WszystkieBadaniaViewModel.cs
+++ MVVMFirma/ViewModels/WszystkieBadaniaViewModel.cs
@@ -1,16 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVMFirma.Models.EntitiesForView;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkieBadaniaViewModel : WorkspaceViewModel
+    public class WszystkieBadaniaViewModel : WszystkieViewModel<BadaniaForAllView>
     {
-        public WszystkieBadaniaViewModel()
+        public WszystkieBadaniaViewModel() : base()
         {
             base.DisplayName = "Wszystkie badania";
+        }
+
+        public override void Load()
+        {
+            List = new ObservableCollection<BadaniaForAllView>(
+                from badania in przychodniaEntities.Badania
+                select new BadaniaForAllView
+                {
+                    BadanieId = badania.BadanieId,
+                    TypBadania = badania.TypBadania,
+                    DataBadania = badania.DataBadania,
+                    PacjentImieNazwisko = badania.Pacjenci.ImieNazwisko,
+                    PacjentDataUrodzenia = badania.Pacjenci.DataUrodzenia,
+                    PacjentMiasto = badania.Pacjenci.Miasto,
+                    LekarzImieNazwisko = badania.Lekarze.ImieNazwisko,
+                    LekarzSpecjalizacja = badania.Lekarze.Specjalizacja,
+                    LekarzNumerTelefonu = badania.Lekarze.NumerTelefonu,
+                }
+                );
         }
     }
 }
