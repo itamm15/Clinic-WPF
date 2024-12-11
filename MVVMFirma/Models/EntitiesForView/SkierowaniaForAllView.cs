8b70df3fbdd3229f8c44e63e6e746f6238d2bb30
itamm15 <osinski.mateusz15@gmail.com>
2024-12-11 21:18:31 +0100

    list all Skierowania with assocs

---
+++ MVVMFirma/Models/EntitiesForView/SkierowaniaForAllView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.EntitiesForView
+{
+    public class SkierowaniaForAllView
+    {
+        public int SkierowanieId { get; set; }
+        public String NumerSkierowania { get; set; }
+        public DateTime? DataWydania { get; set; }
+
+        // Badanie
+        public int? BadanieId { get; set; }
+        public String TypBadania { get; set; }
+        public DateTime? DataBadania { get; set; }
+
+        // Lekarz
+        public String LekarzImieNazwisko { get; set; }
+
+        // Pacjent 
+        public String PacjentImieNazwisko { get; set; }
+        public String PacjentPesel { get; set; }
+    }
+}
--- MVVMFirma/ViewModels/WszystkieSkierowaniaViewModel.cs
+++ MVVMFirma/ViewModels/WszystkieSkierowaniaViewModel.cs
@@ -1,16 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVMFirma.Models.Entities;
+using MVVMFirma.Models.EntitiesForView;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkieSkierowaniaViewModel : WorkspaceViewModel
+    public class WszystkieSkierowaniaViewModel : WszystkieViewModel<SkierowaniaForAllView>
     {
-        public WszystkieSkierowaniaViewModel()
+        public WszystkieSkierowaniaViewModel() : base()
         {
             base.DisplayName = "Wszystkie skierowania";
+        }
+
+        public override void Load()
+        {
+            List = new ObservableCollection<SkierowaniaForAllView>(
+                from skierowanie in przychodniaEntities.Skierowanie
+                select new SkierowaniaForAllView
+                {
+                    SkierowanieId = skierowanie.SkierowanieId,
+                    NumerSkierowania = skierowanie.NumerSkierowania,
+                    DataWydania = skierowanie.DataWydania,
+                    BadanieId = skierowanie.BadanieId,
+                    TypBadania = skierowanie.Badania.TypBadania,
+                    DataBadania = skierowanie.Badania.DataBadania,
+                    LekarzImieNazwisko = skierowanie.Badania.Lekarze.ImieNazwisko,
+                    PacjentImieNazwisko = skierowanie.Badania.Pacjenci.ImieNazwisko,
+                    PacjentPesel = skierowanie.Badania.Pacjenci.Pesel
+                }
+                );
         }
     }
 }
