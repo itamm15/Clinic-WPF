71185bf12527a865440d999f5881947881481d62
itamm15 <osinski.mateusz15@gmail.com>
2024-12-11 20:43:12 +0100

    list all HistoriaChorob with assocs

---
+++ MVVMFirma/Models/EntitiesForView/HistoriaChorobForAllView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.EntitiesForView
+{
+    public class HistoriaChorobForAllView
+    {
+        public int HistoriaChorobyId { get; set; }
+        public String OpisChoroby { get; set; }
+        public DateTime? DataRopoznania { get; set; }
+
+        // Pacjent
+
+        public String PacjentImieNazwisko { get; set; }
+        public DateTime? PacjentDataUrodzenia { get; set; }
+        public String PacjentMiasto { get; set; }
+    }
+}
--- MVVMFirma/ViewModels/WszystkieHistorieChorobViewModel.cs
+++ MVVMFirma/ViewModels/WszystkieHistorieChorobViewModel.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVMFirma.Models.EntitiesForView;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkieHistorieChorobViewModel : WorkspaceViewModel
+    public class WszystkieHistorieChorobViewModel : WszystkieViewModel<HistoriaChorobForAllView>
     {
-        public WszystkieHistorieChorobViewModel()
+        public WszystkieHistorieChorobViewModel() : base()
         {
             base.DisplayName = "Historie chorob";
+        }
+
+        public override void Load()
+        {
+            List = new ObservableCollection<HistoriaChorobForAllView>(
+                from historiaChoroby in przychodniaEntities.HistoriaChorob
+                select new HistoriaChorobForAllView
+                {
+                    HistoriaChorobyId = historiaChoroby.HistoriaChorobyId,
+                    OpisChoroby = historiaChoroby.OpisChoroby,
+                    DataRopoznania = historiaChoroby.DataRozpoznania,
+                    PacjentImieNazwisko = historiaChoroby.Pacjenci.ImieNazwisko,
+                    PacjentDataUrodzenia = historiaChoroby.Pacjenci.DataUrodzenia,
+                    PacjentMiasto = historiaChoroby.Pacjenci.Miasto
+                }
+                );
         }
     }
 }
