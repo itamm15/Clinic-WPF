4a1636de5bc6ea3525eb2dfdfd8051df1177b089
itamm15 <osinski.mateusz15@gmail.com>
2024-12-11 20:51:25 +0100

    list all Platnosc with assocs

---
+++ MVVMFirma/Models/EntitiesForView/PlatnosciForAllView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.EntitiesForView
+{
+    public class PlatnosciForAllView
+    {
+        public int PlatnoscId { get; set; }
+        public int? Kwota { get; set; }
+        public DateTime? DataPlatnosci { get; set; }
+
+        // Pacjent
+        public String PacjentImieNazwisko { get; set; }
+        public DateTime? PacjentDataUrodzenia { get; set; }
+        public String PacjentMiasto { get; set; }
+    }
+}
--- MVVMFirma/ViewModels/WszystkiePlatnosciViewModel.cs
+++ MVVMFirma/ViewModels/WszystkiePlatnosciViewModel.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVMFirma.Models.Entities;
+using MVVMFirma.Models.EntitiesForView;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkiePlatnosciViewModel : WorkspaceViewModel
+    public class WszystkiePlatnosciViewModel : WszystkieViewModel<PlatnosciForAllView>
     {
-        public WszystkiePlatnosciViewModel()
+        public WszystkiePlatnosciViewModel() : base()
         {
             base.DisplayName = "Wszystkie platnosci";
+        }
+
+        public override void Load()
+        {
+            List = new ObservableCollection<PlatnosciForAllView>(
+                from platnosc in przychodniaEntities.Platnosci
+                select new PlatnosciForAllView
+                {
+                    PlatnoscId = platnosc.PlatnoscId,
+                    Kwota = platnosc.Kwota,
+                    DataPlatnosci = platnosc.DataPlatnosci,
+                    PacjentImieNazwisko = platnosc.Pacjenci.ImieNazwisko,
+                    PacjentDataUrodzenia = platnosc.Pacjenci.DataUrodzenia,
+                    PacjentMiasto = platnosc.Pacjenci.Miasto,
+                }
+            );
         }
     }
 }
