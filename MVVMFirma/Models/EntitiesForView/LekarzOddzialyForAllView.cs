9dc9c630b3eae45259efb9b578b1e0f8193aa4e3
itamm15 <osinski.mateusz15@gmail.com>
2024-12-11 23:01:47 +0100

    list all LekarzeOddzialy with assocs

---
+++ MVVMFirma/Models/EntitiesForView/LekarzOddzialyForAllView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.EntitiesForView
+{
+    public class LekarzOddzialyForAllView
+    {
+        public int DoktorOddzialId { get; set; }
+        public DateTime? DataDolaczenia { get; set; }
+
+        // Lekarz
+        public String LekarzImieNazwisko { get; set; }
+        public String LekarzSpecjalizacja { get; set; }
+
+        // Oddzial
+        public String OddzialNazwa {  get; set; }   
+        public String OddzialLokalizacja { get; set; }  
+    }
+}
--- MVVMFirma/ViewModels/WszystkieLekarzOddzialViewModel.cs
+++ MVVMFirma/ViewModels/WszystkieLekarzOddzialViewModel.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVMFirma.Models.EntitiesForView;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkieLekarzOddzialViewModel : WorkspaceViewModel
+    public class WszystkieLekarzOddzialViewModel : WszystkieViewModel<LekarzOddzialyForAllView>
     {
         public WszystkieLekarzOddzialViewModel()
         {
             base.DisplayName = "Lekarz Oddzial";
+        }
+
+        public override void Load()
+        {
+            List = new System.Collections.ObjectModel.ObservableCollection<LekarzOddzialyForAllView>(
+                    from lekarzOddzial in przychodniaEntities.LekarzOddzial
+                    select new LekarzOddzialyForAllView
+                    {
+                        DoktorOddzialId = lekarzOddzial.DoktorOddzialId,
+                        DataDolaczenia = lekarzOddzial.DataDolaczenia,
+                        LekarzImieNazwisko = lekarzOddzial.Lekarze.ImieNazwisko,
+                        LekarzSpecjalizacja = lekarzOddzial.Lekarze.Specjalizacja,
+                        OddzialNazwa = lekarzOddzial.Oddzialy.NazwaOddzialu,
+                        OddzialLokalizacja = lekarzOddzial.Oddzialy.Lokalizacja
+                    }
+                );
         }
     }
 }
