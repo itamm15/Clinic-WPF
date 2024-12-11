bda59fd84baa161f9277e8b82c0d27bf0c010ee4
itamm15 <osinski.mateusz15@gmail.com>
2024-12-11 20:10:39 +0100

    list all Dokumentacje with assocs

---
+++ MVVMFirma/Models/EntitiesForView/DokumentacjaForAllView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.EntitiesForView
+{
+    public class DokumentacjaForAllView
+    {
+        public int DokumentacjaId { get; set; }
+        public String TypDokumentu { get; set; }
+        public DateTime? DataDokumentu { get; set; }
+
+        // Pacjent
+
+        public String PacjentImieNazwisko { get; set; }
+        public DateTime? PacjentDataUrodzenia { get; set; }
+        public String PacjentMiasto { get; set; }
+    }
+}
--- MVVMFirma/ViewModels/WszystkieDokumentacjeViewModel.cs
+++ MVVMFirma/ViewModels/WszystkieDokumentacjeViewModel.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVMFirma.Models.Entities;
+using MVVMFirma.Models.EntitiesForView;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkieDokumentacjeViewModel : WorkspaceViewModel
+    public class WszystkieDokumentacjeViewModel : WszystkieViewModel<DokumentacjaForAllView>
     {
-        public WszystkieDokumentacjeViewModel()
+        public WszystkieDokumentacjeViewModel() : base()
         {
             base.DisplayName = "Cala dokumentacja medyczna";
+        }
+
+        public override void Load()
+        {
+            List = new System.Collections.ObjectModel.ObservableCollection<DokumentacjaForAllView>(
+                from dokumentacja in przychodniaEntities.Dokumentacja
+                select new DokumentacjaForAllView
+                {
+                    DokumentacjaId = dokumentacja.DokumentId,
+                    TypDokumentu = dokumentacja.TypDokumentu,
+                    DataDokumentu = dokumentacja.DataDokumentu,
+                    PacjentImieNazwisko = dokumentacja.Pacjenci.ImieNazwisko,
+                    PacjentDataUrodzenia = dokumentacja.Pacjenci.DataUrodzenia,
+                    PacjentMiasto = dokumentacja.Pacjenci.Miasto
+                }
+                );   
         }
     }
 }
