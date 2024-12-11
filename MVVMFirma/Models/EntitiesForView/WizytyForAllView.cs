4d07e9b6cc0904b1a3c6a303a790375f81cf908c
itamm15 <osinski.mateusz15@gmail.com>
2024-12-11 22:00:44 +0100

    list all Wizyty with assocs

---
+++ MVVMFirma/Models/EntitiesForView/WizytyForAllView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.EntitiesForView
+{
+    public class WizytyForAllView
+    {
+        public int WizytaId { get; set; }
+        public DateTime? DataWizyty { get; set; }
+        
+        // Lekarz
+        public String LekarzImieNazwisko { get; set; }
+
+        // Pacjent
+        public String PacjentImieNazwisko { get; set; }
+        public String PacjentPesel { get; set; }
+    }
+}
--- MVVMFirma/ViewModels/WszystkieWizytyViewModel.cs
+++ MVVMFirma/ViewModels/WszystkieWizytyViewModel.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVMFirma.Models.EntitiesForView;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkieWizytyViewModel : WorkspaceViewModel
+    public class WszystkieWizytyViewModel : WszystkieViewModel<WizytyForAllView>
     {
-        public WszystkieWizytyViewModel()
+        public WszystkieWizytyViewModel() : base()
         {
             base.DisplayName = "Wszystkie wizyty";
+        }
+
+        public override void Load()
+        {
+            List = new ObservableCollection<WizytyForAllView>(
+                from wizyta in przychodniaEntities.Wizyty
+                select new WizytyForAllView
+                {
+                    WizytaId = wizyta.WizytaId,
+                    DataWizyty = wizyta.DataWizyty,
+                    LekarzImieNazwisko = wizyta.Lekarze.ImieNazwisko,
+                    PacjentImieNazwisko = wizyta.Pacjenci.ImieNazwisko,
+                    PacjentPesel = wizyta.Pacjenci.Pesel
+                }
+                );
         }
     }
 }
