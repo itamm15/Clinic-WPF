d0f89780fbff635f3de2da5072516350adef8698
itamm15 <osinski.mateusz15@gmail.com>
2024-12-11 22:27:56 +0100

    list all Recepty with assocs

---
+++ MVVMFirma/Models/EntitiesForView/ReceptyForAllView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.EntitiesForView
+{
+    public class ReceptyForAllView
+    {
+        public int ReceptaId { get; set; }
+        public DateTime? DataWydania { get; set; }
+
+        // Lekarz
+        public String LekarzImieNazwisko { get; set; }  
+
+        // Pacjent
+        public String PacjentImieNazwisko { get; set; }
+        public String PacjentPesel { get; set; }
+    }
+}
--- MVVMFirma/ViewModels/WszystkieReceptyViewModel.cs
+++ MVVMFirma/ViewModels/WszystkieReceptyViewModel.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVMFirma.Models.EntitiesForView;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkieReceptyViewModel : WorkspaceViewModel
+    public class WszystkieReceptyViewModel : WszystkieViewModel<ReceptyForAllView>
     {
-        public WszystkieReceptyViewModel()
+        public WszystkieReceptyViewModel() : base()
         {
             base.DisplayName = "Wszystkie recepty";
+        }
+
+        public override void Load()
+        {
+            List = new ObservableCollection<ReceptyForAllView>(
+                from recepta in przychodniaEntities.Recepty
+                select new ReceptyForAllView
+                {
+                    ReceptaId = recepta.ReceptaId,
+                    DataWydania = recepta.DataWydania,
+                    LekarzImieNazwisko = recepta.Lekarze.ImieNazwisko,
+                    PacjentImieNazwisko = recepta.Pacjenci.ImieNazwisko,
+                    PacjentPesel = recepta.Pacjenci.Pesel,
+                }
+            );
         }
     }
 }
