99bc849b1ca46f7bf219819f6ead6894fa7aff8b
itamm15 <osinski.mateusz15@gmail.com>
2024-12-12 18:08:11 +0100

    list all PersonelOddzialy with assocs

---
+++ MVVMFirma/Models/EntitiesForView/PersonelOddzialForAllView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.EntitiesForView
+{
+    public class PersonelOddzialForAllView
+    {
+        public int PersonelOddzialId { get; set; }
+        public DateTime? DataDolaczenia { get; set; }   
+
+        // Oddzial
+        public String OddzialNazwaOddzialu { get; set; }
+        public String OddzialLokalizacja { get; set; }
+        public DateTime? OddzialDataOtwarcia { get; set; }
+
+        // Personel
+        public String PersonelImieNazwisko { get; set; }
+        public String PersonelStanowisko { get; set; }
+    }
+}
--- MVVMFirma/ViewModels/WszystkiePersonelOddzialViewModel.cs
+++ MVVMFirma/ViewModels/WszystkiePersonelOddzialViewModel.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVMFirma.Models.EntitiesForView;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkiePersonelOddzialViewModel : WorkspaceViewModel
+    public class WszystkiePersonelOddzialViewModel : WszystkieViewModel<PersonelOddzialForAllView>
     {
-        public WszystkiePersonelOddzialViewModel()
+        public WszystkiePersonelOddzialViewModel() : base()
         {
             base.DisplayName = "Personel oddzial";
+        }
+
+        public override void Load()
+        {
+            List = new System.Collections.ObjectModel.ObservableCollection<PersonelOddzialForAllView>(
+                from personelOddzial in przychodniaEntities.PersonelOddzial
+                select new PersonelOddzialForAllView
+                {
+                    PersonelOddzialId = personelOddzial.PersonelOddzial1,
+                    DataDolaczenia = personelOddzial.DataDolaczenia,
+                    OddzialNazwaOddzialu = personelOddzial.Oddzialy.NazwaOddzialu,
+                    OddzialLokalizacja = personelOddzial.Oddzialy.Lokalizacja,
+                    OddzialDataOtwarcia = personelOddzial.Oddzialy.DataOtwarcia,
+                    PersonelImieNazwisko = personelOddzial.Personel.ImieNazwisko,
+                    PersonelStanowisko = personelOddzial.Personel.Stanowisko
+                }
+                );
         }
     }
 }
