3d5497525c32cf12c1c4f1f207b86ee6740ee7be
itamm15 <osinski.mateusz15@gmail.com>
2024-12-13 20:49:50 +0100

    add with foreign key Badania

--- MVVMFirma/ViewModels/NoweBadanieViewModel.cs
+++ MVVMFirma/ViewModels/NoweBadanieViewModel.cs
@@ -3,14 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVMFirma.Models.BusinessLogic;
+using MVVMFirma.Models.Entities;
+using MVVMFirma.Models.EntitiesForView;
 
 namespace MVVMFirma.ViewModels
 {
-    public class NoweBadanieViewModel : WorkspaceViewModel
+    public class NoweBadanieViewModel : JedenViewModel<Badania>
     {
-        public NoweBadanieViewModel()
+        public NoweBadanieViewModel() : base("Nowe badanie")
         {
-            base.DisplayName = "Nowe badanie";
+            item = new Badania();
+        }
+
+        // fields 
+        public String TypBadania
+        {
+            get
+            {
+                return item.TypBadania;
+            }
+            set
+            {
+                item.TypBadania = value;
+                OnPropertyChanged(() =>  TypBadania);
+            }
+        }
+
+        public DateTime? DataBadania
+        {
+            get
+            {
+                return item.DataBadania;
+            }
+            set
+            {
+                item.DataBadania = value;
+                OnPropertyChanged(() => DataBadania);
+            }
+        }
+
+        public int? LekarzId
+        {
+            get
+            {
+                return item.LekarzId;
+            }
+            set
+            {
+                item.LekarzId = value;
+                OnPropertyChanged(() => LekarzId);
+            }
+        }
+
+        public int? PacjentId
+        {
+            get
+            {
+                return item.PacjentId;
+            }
+            set
+            {
+                item.PacjentId = value;
+                OnPropertyChanged(() => PacjentId);
+            }
+        }
+
+        // Combobox
+        public IQueryable<KeyAndValue> LekarzItems
+        {
+            get
+            {
+                return new LekarzB(przychodniaEntities).GetLekarzKeyAndValueItems();
+            }
+        }
+
+        public IQueryable<KeyAndValue> PacjentItems
+        {
+            get
+            {
+                return new PacjentB(przychodniaEntities).GetPacjentKeyAndTowarItems();
+            }
+        }
+
+        public override void Save()
+        {
+            przychodniaEntities.Badania.Add(item);
+            przychodniaEntities.SaveChanges();
         }
     }
 }
--- MVVMFirma/Views/NoweBadanieView.xaml.cs
+++ MVVMFirma/Views/NoweBadanieView.xaml.cs
@@ -18,7 +18,7 @@ namespace MVVMFirma.Views
     /// <summary>
     /// Logika interakcji dla klasy NoweBadanieView.xaml
     /// </summary>
-    public partial class NoweBadanieView : UserControl
+    public partial class NoweBadanieView : JedenViewBase
     {
         public NoweBadanieView()
         {
